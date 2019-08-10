9b4af82637aad015a4dfc03dd6296f61ff96b74e
lucki90 <tomlucki@wp.pl>
2019-08-10 13:37:03 +0200

    init

---
+++ src/main/java/pl/lucky/TrainignValidationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.lucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainignValidationSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainignValidationSpringBootApplication.class, args);
+    }
+
+}
